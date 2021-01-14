1fd0ae767baa5cd31c3420f4318b103c1ea97fc5
suvadeepCN2020 <suvadeep@capitalnumbers.com>
2021-01-14 07:38:50 +0530

    first commit pwa

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <main>
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+  ul {
+    list-style: none;
+    display: flex;
+    padding: 0;
+  }
+  li {
+    padding: 5px 15px 5px 0;
+  }
+  li a {
+    text-decoration: none;
+    color: black;
+  }
+  li a:hover {
+    color: #404040;
+  }
+</style>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      {
+        name: 'Home',
+        to: '/'
+      },
+      {
+        name: 'Page 1',
+        to: '/page-1'
+      },
+      {
+        name: 'Bad Link',
+        to: '/random-bad-url'
+      }
+    ]
+  })
+}
+</script>
